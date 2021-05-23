9866ec6db329070d3b8f99f3ff6cedcec351574c
khanh.luong <baokhanh2710cntt@gmail.com>
2021-05-23 20:31:31 +1200

    add provirder.

--- lib/screens/sign_in/sign_in_page.dart
+++ lib/screens/sign_in/sign_in_page.dart
@@ -14,14 +14,18 @@ class SignPage extends StatelessWidget {
 
   static Widget create(BuildContext context) {
     final auth = Provider.of<AuthBase>(context, listen: false);
-    return Provider<SignInBloc>(
-        create: (_) => SignInBloc(auth: auth),
-        dispose: (__, bloc) => bloc.dispose(),
-        child: Consumer<SignInBloc>(
-          builder: (_, bloc, __) => SignPage(
-            bloc: bloc,
-          ),
-        ));
+    return ChangeNotifierProvider<ValueNotifier<bool>>(
+      create: (_) => ValueNotifier<bool>(false),
+      child: Consumer<ValueNotifier<bool>>(
+        builder: (_, isLoading, __) => Provider<SignInBloc>(
+            create: (_) => SignInBloc(auth: auth, isLoading: isLoading),
+            child: Consumer<SignInBloc>(
+              builder: (_, bloc, __) => SignPage(
+                bloc: bloc,
+              ),
+            )),
+      ),
+    );
   }
 
   void _showSignInError(BuildContext context, Exception exception) {
@@ -69,18 +73,13 @@ class SignPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final bloc = Provider.of<SignInBloc>(context, listen: false);
+    final isLoading = Provider.of<ValueNotifier<bool>>(context);
     return Scaffold(
       appBar: AppBar(
         title: Text('Time Tracker'),
         elevation: 2.0,
       ),
-      body: StreamBuilder<bool>(
-          stream: bloc.isLoadingStream,
-          initialData: false,
-          builder: (context, snapshot) {
-            return _buildContent(context, snapshot.data);
-          }),
+      body: _buildContent(context, isLoading.value),
       backgroundColor: Colors.grey[200],
     );
   }
